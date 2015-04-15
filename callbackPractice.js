c90a4dcfa73a66695e1adddfa65953cc2de2a857
Kristaphonie <csaunders@getjive.com>
2015-04-14 21:11:54 -0600

    finished projectomundo

--- callbackPractice.js
+++ callbackPractice.js
@@ -27,9 +27,16 @@ and what you should write is the favNum function that makes the code above work,
   //Code Here for first
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function first (a, b)
+{
+  b(a);
+}
+
 first(names, function(firstName){
-  console.log('The first name in names is ', firstName)
+  console.log('The first name in names is ', firstName[0]);
 });
+
 
 
 
@@ -41,8 +48,14 @@ first(names, function(firstName){
   //Code Here for last
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function last (a, b)
+{
+  b(a)
+}
+
 last(names, function(lastName){
-  console.log('The last name in names is ', lastName);
+  console.log('The last name in names is ', lastName[lastName.length - 1]);
 });
 
 
@@ -57,6 +70,11 @@ last(names, function(lastName){
 
 
   //Code Here for multiply
+
+function multiply (a, b, c)
+{
+  c(a * b)
+}
 
 multiply(4, 3, function(answer){
   console.log('The answer is ', answer); //should console.log 12
@@ -75,6 +93,19 @@ multiply(4, 3, function(answer){
   //Code Here for contains
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function contains (a, b, c)
+{
+  if (a.indexOf(b))
+  {
+    c(true);
+  }
+  else
+  {
+    c(false)
+  }
+}
+
 contains(names, 'Colt', function(result){
   if(result === true){
     console.log('Colt is in the array');
@@ -95,7 +126,24 @@ contains(names, 'Colt', function(result){
     //Code Here for uniq
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+function uniq (a, b)
+{
+  for(var i = 0; i < a.length; i++)
+  {
+    for(var o = 0 + 1; o < a.length; o++)
+    {
+      if(a[i] === a[o])
+      {
+        a.splice(i, 1)
+        b(a)
+        o--;
+      }
+    }
+  }
+}
+
 uniq(names, function(uniqArr){
+
   console.log('The new names array with all the duplicate items removed is ', uniqArr);
 });
 
@@ -111,12 +159,15 @@ uniq(names, function(uniqArr){
     //Code Here for each
 
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+
+function each (a, b)
+{
+  b(a[0], a.indexOf('Tyler'))
+}
+
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item)
 });
-
-
-
 
 
 /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
@@ -147,6 +198,17 @@ var users = [
     address: '192 East 32 North'
   },
 ];
+
+function getUserById (a, b, c)
+{
+  for (var i = 0; i < users.length; i++)
+  {
+    if (a[i].id === b)
+    {
+      c(a[i]);
+    }
+  }
+}
 
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
